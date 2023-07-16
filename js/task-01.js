8d9eab0310e3ac2668ec814f69a2f010f9d12a3d
wosean <serhii_v@ukr.net>
2023-07-17 00:41:51 +0300

    add files

--- js/task-01.js
+++
@@ -1,8 +0,0 @@
-const elementsCategories = document.getElementById('categories');
-console.log(`Number of categories: ${elementsCategories.children.length}`);
-
-const itemsElement = document.querySelectorAll(".item");
-itemsElement.forEach((item) => {
-    console.log("Category: ", item.firstElementChild.textContent);
-    console.log("Elements: ", item.lastElementChild.children.length);
-  });
--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,3 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-const ingredientsList = document.querySelector('#ingredients');
-const itemsList = ingredients.map((ingredient) => {
-const item = document.createElement('li');
-  item.textContent = ingredient;
-  item.classList.add('item');
-  return item;
-});
-ingredientsList.append(...itemsList);
